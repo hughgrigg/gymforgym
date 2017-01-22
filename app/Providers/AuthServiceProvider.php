c244eed0c4053bb607570c03bd18b0b86a7adcc6
Hugh Grigg <hugh@loveknitting.com>
2017-01-22 12:54:31 +0000

    Apply fixes from StyleCI

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace GymForGym\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
